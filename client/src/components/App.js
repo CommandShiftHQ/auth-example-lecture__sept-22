be837928776748514cbd50097c66c5a758211c52
devarrowsmith <devarrowsmith@gmail.com>
2022-11-30 22:58:43 +0000

    Return server responses as a JWT bundled in a cookie. Access this on the frontend to immediately update context, and retrieve the cookie from memory to persist a user session.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,23 @@
 import "../styles/app.css";
 import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AuthContext from "../utils/AuthContext";
 import Home from "./Home";
 import Account from "./Account";
 import Login from "./Login";
+import Cookie from "js-cookie";
+import jwtDecode from "jwt-decode";
 
 const App = () => {
   const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const token = Cookie.get("userToken");
+    if (token) {
+      const currentUser = jwtDecode(token);
+      setUser(currentUser);
+    }
+  }, []);
 
   return (
     <AuthContext.Provider value={{ user, setUser }}>
--- server/routers/auth.js
+++ server/routers/auth.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const { getUserLogin } = require("../db/queries");
 
 require('dotenv').config();
+const jwtSecret = process.env.jwtSecret;
 
 router.post("/login", async (req, res, next) => {
   const { username, email, password } = req.body
 
-  await getUserLogin({ username, email, password })
+  await getUserLogin({ username, email })
     .then(result => {
       console.log('getUserLogin result:', result);
 
@@ -22,12 +24,14 @@ router.post("/login", async (req, res, next) => {
       bcrypt
         .compare(password, result.password)
         .then(result => {
-          if (result = true) {
-
-            res.status(201)
+          if (result === true) {
+            const token = jwt.sign({ username, email }, jwtSecret);
+            res
+              .status(201)
+              .cookie('userToken', token, { maxAge: 1000 * 60 * 60 * 24 })
               .send({
                 status: 201,
-                message: "Correct password used"
+                message: "Correct password used, cookie created"
               })
           } else {
             res.status(401).send({
